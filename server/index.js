d6d1d0f38256386f4a50fc4bad1bed2e316177d8
Pedro Duarte <contact@peduarte.com>
2015-08-17 20:17:09 +0100

    Update package and readme

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-var app = express();
 var fs = require('fs');
 var request = require('request');
 var marked = require('marked');
+var argv = require('yargs').argv;
 
-var PORT = process.env.PORT || 8080;
 var OUTPUT_PATH = 'client/src/data/';
-
-app.use(cors());
 
 function scrapeReadme(mdUrl, callback) {
   request(mdUrl, function (error, response, html) {
@@ -19,25 +16,15 @@ function scrapeReadme(mdUrl, callback) {
 
       fs.writeFile(OUTPUT_PATH + 'readme.json', JSON.stringify(parsedResult, null, 4), function(err){
 				if (err) { return console.log(err); }
-	      return callback();
+	      console.log('Your file is ready – ' + OUTPUT_PATH + 'readme.json');
       });
     }
   });
 }
 
-app.get('/', function(req, res) {
-	res.send('Nothing to see here ¯\\_(ツ)_/¯');
-});
 
-app.get('/api/readme', function(req, res) {
-  if (req.query.mdUrl) {
-    scrapeReadme(req.query.mdUrl, function(data) {
-    	console.log('File successfully written!');
-      res.send('File successfully written!')
-    });
-  }
-});
-
-
-app.listen(PORT);
-console.log('Listening at localhost:' + PORT);
+if (argv.url) {
+	scrapeReadme(argv.url);
+} else {
+	console.log('You must pass a url via the --url flag');
+}
