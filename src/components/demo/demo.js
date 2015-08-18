90718d236f04c91a9d55c743931531f4e4649469
Pedro Duarte <contact@peduarte.com>
2015-08-18 16:13:28 +0100

    Update demo with credit link

--- src/components/demo/demo.js
+++ src/components/demo/demo.js
@@ -14,22 +14,25 @@ import img5 from '../../images/fuzzy3.gif';
 var wallop;
 
 export default React.createClass({
-	componentDidMount() {
-		wallop = new Wallop(document.querySelector('.Wallop'));
-	hljs.initHighlightingOnLoad();
-	},
+  componentDidMount() {
+    wallop = new Wallop(document.querySelector('.Wallop'));
+  hljs.initHighlightingOnLoad();
+  },
 
   render() {
     return (
-      <div className="Wallop">
-    		<div className="Wallop-item Wallop-item--current"><img src={img1}/></div>
-        <div className="Wallop-item"><img src={img2}/></div>
-        <div className="Wallop-item"><img src={img3}/></div>
-        <div className="Wallop-item"><img src={img4}/></div>
-    		<div className="Wallop-item"><img src={img5}/></div>
-    		<button className="Wallop-buttonPrevious">Previous</button>
-    		<button className="Wallop-buttonNext">Next</button>
-    	</div>
+      <div className="Demo">
+        <div className="Wallop">
+          <div className="Wallop-item Wallop-item--current"><img src={img1}/></div>
+          <div className="Wallop-item"><img src={img2}/></div>
+          <div className="Wallop-item"><img src={img3}/></div>
+          <div className="Wallop-item"><img src={img4}/></div>
+          <div className="Wallop-item"><img src={img5}/></div>
+          <button className="Wallop-buttonPrevious">Previous</button>
+          <button className="Wallop-buttonNext">Next</button>
+        </div>
+        <p>Images courtesy of <a href="https://dribbble.com/TonyBabel">TonyBabel</a></p>
+      </div>
     )
   }
 });
