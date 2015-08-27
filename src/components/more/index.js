c3d84b0c567eb489ad6d30bf1b1ab0e9748c8e6e
Pedro Duarte <contact@peduarte.com>
2015-08-27 10:50:33 +0100

    Update website with GH lin

--- src/components/more/index.js
+++ src/components/more/index.js
@@ -10,6 +10,8 @@ export default React.createClass({
     return (
       <div className="More">
         <div className="Container">
+          <h3>About this site</h3>
+          <p>This site is built using webpack, React and mdhtmljson for the documentation. Check out the project on <a href="https://github.com/peduarte/wallop-site" target="_blank">github</a>!</p>
           <h3>More</h3>
           <p>Please check the <a href={readmeUrl}>README</a> for more information on the API, contributing and license.</p>
           <p>Images courtesy of <a href="https://dribbble.com/TonyBabel">TonyBabel</a></p>
